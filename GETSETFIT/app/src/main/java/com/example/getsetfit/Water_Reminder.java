c7276ab89f0d6040f40c30656801d34accc340af
pallavee-2705 <87165168+pallavee-2705@users.noreply.github.com>
2022-05-09 17:28:29 +0530

    Add files via upload

---
+++ GETSETFIT/app/src/main/java/com/example/getsetfit/Water_Reminder.java
@@ -0,0 +1,14 @@
+package com.example.getsetfit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Water_Reminder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_water_reminder);
+    }
+}
---
+++ GETSETFIT/app/src/main/java/com/example/getsetfit/leg_workout.java
@@ -0,0 +1,41 @@
+package com.example.getsetfit;
+
+import android.os.Bundle;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import com.example.getsetfit.databinding.ActivityLegWorkoutBinding;
+
+public class leg_workout extends AppCompatActivity {
+
+    private ActivityLegWorkoutBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityLegWorkoutBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Toolbar toolbar = binding.toolbar;
+        setSupportActionBar(toolbar);
+        CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
+        toolBarLayout.setTitle(getTitle());
+
+        FloatingActionButton fab = binding.fab;
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
